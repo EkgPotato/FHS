0657292676a9c81f50338755805bcd48c900437a
EkgPotato <130781423+EkgPotato@users.noreply.github.com>
2024-11-20 21:37:33 +0100

    Moved from kotlin to MAUI

---
+++ FHS.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FHS.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
